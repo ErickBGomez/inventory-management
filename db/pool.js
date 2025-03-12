90545aa91394a1ec763572865b768da6405f585c
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-12 14:57:02 -0600

    Setup basic connection with database

---
+++ db/pool.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const { CONNECTION_STRING } = process.env;
+
+module.exports = new Pool({
+  connectionString: CONNECTION_STRING,
+});
---
+++ db/queries.js
@@ -0,0 +1,5 @@
+const pool = require("./pool");
+
+// Define queries here
+
+// module.exports = {};
